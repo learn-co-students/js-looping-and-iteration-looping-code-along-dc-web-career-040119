585a9c111589edd2a066a2925df98b508eee720b
Matthew <mf9646a@alumni.american.edu>
2019-05-14 12:49:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countdown(int){
+  for(int; int > -1; int--)
+  console.log(int)
+}
+
+
+function writeCards(arr, event){
+  let newarr = []
+  for(let i = 0; i < arr.length; i++) {
+  newarr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+  }
+  return newarr
+}
