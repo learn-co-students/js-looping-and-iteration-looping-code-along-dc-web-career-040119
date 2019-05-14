2e6f8f885c82ac5738c2f8b0ce8685eefdc17344
github username <github email address>
2019-05-14 12:25:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const writeCards = (names, event) => {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+const countdown = (n) => {
+    while(n>=0) {
+        console.log(n);
+        n--
+    }
+}
