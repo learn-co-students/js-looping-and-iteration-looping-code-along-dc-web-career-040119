087938c80761caa8527d364392d5cc0c2d7d6aef
Matthew Lindsey <mbl@null.net>
2019-05-21 15:31:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const writeCards = (names, occassion) => {
+  const cards = []
+  for (let i = 0; i < names.length; i += 1) {
+    const card = `Thank you, ${names[i]}, for the wonderful ${occassion} gift!`
+    cards.push(card)
+  }
+  return cards
+}
+
+const countdown = number => {
+  let counter = number
+  while (counter >= 0) {
+    console.log(counter)
+    counter -= 1
+  }
+}
